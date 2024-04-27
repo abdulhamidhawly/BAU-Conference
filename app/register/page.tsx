d6426721cf2747aed44d7da83af1c9570b68336c
Mustafa1121 <mustafa.khodor11@gmail.com>
2024-04-27 16:24:29 +0300

    fix form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -90,20 +90,10 @@ const Form = () => {
 
   return (
     <>
-      <div className="font-[sans-serif] text-gray-800 bg-white max-w-4xl flex items-center mx-auto md:h-screen p-4 mb-12">
-        <div className="grid md:grid-cols-3 items-center shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-xl overflow-hidden">
-          {/* <div className="max-md:order-1 flex flex-col justify-center space-y-16 max-md:mt-16 min-h-full bg-gradient-to-r from-gray-900 to-gray-700 lg:px-8 px-4 py-4">
-            <div>
-              <h4 className="text-white text-lg font-semibold text-center">
-                BAU Registration
-              </h4>
-              <p className="text-[13px] text-white mt-2 text-center">
-                Welcome to our registration page! Get started by creating your
-                account.
-              </p>
-            </div>
-          </div> */}
-          <form className="md:col-span-2 w-full py-6 px-6 sm:px-16">
+      <div className="font-[sans-serif] text-gray-800 bg-white max-w-4xl flex justify-center items-center mx-auto md:h-screen p-4 mb-12">
+        <div className="items-center shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-xl overflow-hidden">
+         
+          <form className=" w-full py-6 px-6 sm:px-16">
             <div className="mb-6">
               <h3 className="text-lg font-bold text-center">
                 Immigrant Empowerment Conference Registration
